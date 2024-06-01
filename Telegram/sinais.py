59fda6b6ffd70133c65a493900f31bd933dfd99f
454544333344 <mussarelo643@gmail.com>
2024-06-01 17:04:04 -0300

    Update sinais.py

--- Telegram/sinais.py
+++ Telegram/sinais.py
@@ -1,6 +1,6 @@
-API_TOKEN = "SEU_TOKEN"
-CHAT_ID = "ID DO SEU CHAT/GRUPO"
-URL = "https://blaze-4.com/api/crash_games/recent"
+API_TOKEN = "7413418087:AAFa6s3SHansZSn4ZomK6v1pTWFxYZejGKM"
+CHAT_ID = "6915234050"
+URL = "https://777xbets.com/vgames/exclusive/fortunetiger"
 
 import json
 import requests
@@ -9,7 +9,7 @@ import telebot
 bot = telebot.TeleBot(API_TOKEN)
 
 
-def blaze():
+def 777xbets():
     resposta = requests.get(URL)
     return resposta.text
 
@@ -23,16 +23,16 @@ def pegar_resultado(resposta):
     json_valores = json.loads(resposta)
     for index, valor in enumerate(json_valores):
         if index < 4:
-            if float(valor["crash_point"]) >= 2.0:
-                lista_resultados.append(valor["crash_point"])
-                print("bom", valor["crash_point"])
+            if float(valor["fortune_tiger"]) >= 2.0:
+                lista_resultados.append(valor["fortune_tiger"])
+                print("bom", valor["fortune_tiger"])
             else:
-                print("ruim", valor["crash_point"])
+                print("ruim", valor["fortune_tiger"])
     return lista_resultados
 
 
 if __name__ == "__main__":
-    resposta = blaze()
+    resposta = 777xbets()
     lista_resultados = pegar_resultado(resposta)
     if len(lista_resultados) > 1:
-        telegram("Entrada no jogo crash")
+        telegram("Entrada no jogo tiger")
